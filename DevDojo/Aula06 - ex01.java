f969ec7f3d4776c1e12ed4b31e65e0e741def443
Darlley <37590954+Darlley@users.noreply.github.com>
2018-06-30 17:37:06 -0400

    Delete Aula06 - ex01.java

--- DevDojo/Aula06 - ex01.java
+++
@@ -1,9 +0,0 @@
-public Class RelatorioPendencia {
-    public static void main (String[] args) {
-        String nome = "Fulano";
-        String endereco = "Rua dos bobos"; 
-        String telefone = "4002-8922";
-        
-        System.out.println("O " + nome + ", domiciliado no endereço " + endereco + ", e telefone" + telefone + ", não possui nenhum tipo de pendência.");
-    }
-}
