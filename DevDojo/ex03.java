324a35d88ae97cd29979d21a29157e41fbec8c09
Darlley <37590954+Darlley@users.noreply.github.com>
2018-06-30 18:47:12 -0400

    Create ex03.java

---
+++ DevDojo/ex03.java
@@ -0,0 +1,10 @@
+public class ex04 {
+    public static void main(String[] args) {
+        int count = 100001;
+        for(int i = 0; i < count; i++){
+            if(i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
