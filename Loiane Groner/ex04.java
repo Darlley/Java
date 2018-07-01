b82c9adabe622d7ab85b50564bbbcc66b2babdbd
Darlley <37590954+Darlley@users.noreply.github.com>
2018-07-01 13:35:50 -0400

    Create ex04.java

---
+++ Loiane Groner/ex04.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class NumeroRetornado {
+  puclic static void main(String[] args){
+    
+    Scanner scan = new Scanner(System.in);
+    
+    System.out.println("Informe a primeira nota: ");
+    double nota1 = scan.nextDouble();
+    System.out.println("Informe a segunda nota: ");
+    double nota2 = scan.nextDouble();
+    System.out.println("Informe a terceira nota: ");
+    double nota3 = scan.nextDouble();
+    System.out.println("Informe a quarta nota: ");
+    boolean nota4 = scan.nextDouble();
+    
+    double media = (nota1 + nota2 + nota3 + nota4)/4;
+    
+    System.out.println("O numero informado foi " + media);
+  }
+}
