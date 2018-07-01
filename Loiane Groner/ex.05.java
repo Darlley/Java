6a7ee2414896493c500c85acd06cff8edc9626d2
Darlley <37590954+Darlley@users.noreply.github.com>
2018-07-01 13:37:40 -0400

    Create ex.05.java

---
+++ Loiane Groner/ex.05.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class CentimetrosParaMetros {
+  puclic static void main(String[] args){
+    
+    Scanner scan = new Scanner(System.in);
+    
+    System.out.println("Quantos metros quer converter: ");
+    double metros = scan.nextDouble();
+    
+    
+    double centimetros = metros/100;
+    
+    System.out.println("O numero informado foi " + centrimetros);
+  }
+}
