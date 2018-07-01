10fcc1307ed6ad66449b145b1fd4e7e46743ef55
Darlley <37590954+Darlley@users.noreply.github.com>
2018-07-01 13:29:42 -0400

    Create ex.02.java

---
+++ Loiane Groner/ex.02.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class NumeroRetornado {
+  puclic static void main(String[] args){
+    
+    Scanner scan = new Scanner(System.in);
+    
+    System.out.println("Digite um número: ");
+    int numero = scan.nextInt();
+    System.out.println("O numero informado foi " + numero);
+  }
+}
