9670efc200e40e71bbf05a392b704471569d4c2a
Darlley <37590954+Darlley@users.noreply.github.com>
2018-07-02 12:40:49 -0400

    Create ex.01.java

---
+++ Loiane Groner/Segunda Lista/ex.01.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class MaiorNumero {
+
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Informe um número: ");
+        int umNumero  = scan.nextInt();
+
+        System.out.println("Informe outro número: ");
+        int outroNumero  = scan.nextInt();
+
+        if(umNumero > outroNumero){
+            System.out.println("O maior número é " + umNumero);
+        }else{
+            System.out.println("O maior número é " + outroNumero);
+        }
+    }
+}
