d42d535b58a49656e1281b96821918fc6be05487
Darlley <37590954+Darlley@users.noreply.github.com>
2018-07-02 13:23:42 -0400

    Create ex05.java

---
+++ Loiane Groner/Segunda Lista/ex05.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ex01 {
+
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Informe a primeira nota:");
+        double umaNota = scan.nextDouble();
+        System.out.println("Informe a segunda nota:");
+        double outraNota = scan.nextDouble();
+
+        double media = (umaNota + outraNota)/2;
+
+        if(media < 7 ){
+            System.out.println("Reprovado");
+        }else if(media >= 7){
+                System.out.println("Aprovado");
+        }else if(media == 10) {
+            System.out.println("Aprovado com distinção");
+        }
+    }
+}
