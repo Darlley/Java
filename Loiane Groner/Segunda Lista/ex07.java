6984b7bb368f93abb14e92bbee9801690bb89f5e
Darlley <37590954+Darlley@users.noreply.github.com>
2018-07-05 11:46:01 -0400

    Create ex07.java

---
+++ Loiane Groner/Segunda Lista/ex07.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ex07 {
+
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Informe um número: ");
+        int umNumero  = scan.nextInt();
+
+        System.out.println("Informe outro número: ");
+        int outroNumero  = scan.nextInt();
+
+        System.out.println("Informe mais um número: ");
+        int maisUmNumero  = scan.nextInt();
+
+        if(umNumero > outroNumero && outroNumero > maisUmNumero){
+            System.out.println("O menor número é " + maisUmNumero);
+        }else if(umNumero > outroNumero && outroNumero < maisUmNumero){
+            System.out.println("O menor número é " + outroNumero);
+        }else{
+            System.out.println("O menor número é " + umNumero);
+        }
+    }
+}
