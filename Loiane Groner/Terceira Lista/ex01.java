e1dc6c31cb903e72970a0ad73101d19954c38afa
Darlley <37590954+Darlley@users.noreply.github.com>
2018-07-06 10:20:56 -0400

    Update ex01.java

--- Loiane Groner/Terceira Lista/ex01.java
+++ Loiane Groner/Terceira Lista/ex01.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class ex01 {
 
     public static void main(String[] args){
-        //peça nota entre 0 a 10, mensagem de erro caso valor maior que 10, continue pedindo ate o valor ser valido
+
         Scanner scan = new Scanner(System.in);
 
         String mensagem = "Informe uma nota entre 0 e 10: ";
