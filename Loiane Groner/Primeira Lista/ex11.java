4533ab4dd67bc283db485c4dce537d394a2776ab
Darlley <37590954+Darlley@users.noreply.github.com>
2018-07-07 11:18:32 -0400

    Update ex11.java

--- Loiane Groner/Primeira Lista/ex11.java
+++ Loiane Groner/Primeira Lista/ex11.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ProdutosResultantes {
+public class ex11 {
 
     public static void main(String[] args){
 
@@ -13,12 +13,12 @@ public class ProdutosResultantes {
         System.out.println("Agora informe um numero real: ");
         double real  = scan.nextDouble();
 
-        double ProdutoA = (inteiro1*2) + (inteiro2/2);
-        double ProdutoB = (inteiro1*3) + (real*3);
+        int ProdutoA = (inteiro1 * 2) * (inteiro2 / 2);
+        double ProdutoB = (inteiro1 * 3) + real;
         double ProdutoC = Math.pow(real,3);
 
-        System.out.println("O produto da soma do dobro do primeiro com o segundo é " + ProdutoA);
-        System.out.println("O produto da soma entre o tripro do primeiro e do segundo é " + ProdutoB);
-        System.out.println("O produto do terceiro elevado à terceira potencia é " + ProdutoC);
+        System.out.println("O produto do dobro do primeiro com metade do segundo é " + ProdutoA);
+        System.out.println("O produto da soma entre o tripro do primeiro com o terceiro é " + ProdutoB);
+        System.out.println("O produto do terceiro elevado ao cubo " + ProdutoC);
     }
 }
