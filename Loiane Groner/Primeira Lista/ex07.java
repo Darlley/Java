b92f587cb35b0bd8df7155c67d62cf1bb7016abd
Darlley <37590954+Darlley@users.noreply.github.com>
2018-07-07 11:06:37 -0400

    Update ex07.java

--- Loiane Groner/Primeira Lista/ex07.java
+++ Loiane Groner/Primeira Lista/ex07.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
 
-public class DobroArea {
+public class ex07 {
     puclic static void main(String[] args){
 
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Informe o comprimento de um lado: ");
         double lado = scan.nextDouble();
-        double area = lado * lado;
+        
+        //double area = lado * lado;
+        double area = Math.pow(lado, 2);
         double dobroArea = area * 2;
 
+        System.out.println("A área do quadrado é " + area);
         System.out.println("O dobro da área é " + dobroArea);
     }
 }
