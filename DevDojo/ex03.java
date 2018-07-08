df0615d3728f8927f97937337ae9033e2e9736f9
Darlley <37590954+Darlley@users.noreply.github.com>
2018-07-08 15:31:08 -0400

    CORRIGIDO ✔

--- DevDojo/ex03.java
+++ DevDojo/ex03.java
@@ -1,7 +1,9 @@
+// AULA 16
+
 public class ex04 {
     public static void main(String[] args) {
-        int count = 100001;
-        for(int i = 0; i < count; i++){
+        int counter = 100001;
+        for(int i = 0; i < counter; i++){
             if(i % 2 == 0){
                 System.out.println(i);
             }
