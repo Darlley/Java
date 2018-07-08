104ec770f952eb77f12e30d75410a68d4844f5de
Darlley <37590954+Darlley@users.noreply.github.com>
2018-07-08 15:14:59 -0400

    CORRIGIDO ✔

--- DevDojo/ex01.java
+++ DevDojo/ex01.java
@@ -3,18 +3,19 @@ public class ex01 {
 
         //VARIAVEIS
         double salario = 3500;
-        double imposto;
+        double imposto = 0;
 
         //INICIO
         if(salario < 1000){
-            imposto = salario*0.05;
-        } else if (salario >= 1000 && salario < 2000) {
-            imposto = salario*0.1;
-        } else if (salario >= 2000 && salario <= 4000) {
-            imposto = salario*0.15;
-        } else {
-            imposto = salario*0.2;
+            imposto = salario * 0.05;
+        }else if(salario >= 1000 && salario < 2000) {
+            imposto = salario * 0.1;
+        }else if(salario >= 2000 && salario < 4000) {
+            imposto = salario * 0.15;
+        }else{
+            imposto = salario * 0.2;
         }
+        
         System.out.println("O imposto é de: " + imposto);
     }
 }
