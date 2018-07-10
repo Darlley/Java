0d71a7f66e6135c67b58179ba100549ebc911668
Darlley <37590954+Darlley@users.noreply.github.com>
2018-07-10 10:02:12 -0400

    CORRIGIDO ✔

--- Loiane Groner/Segunda Lista/ex11.java
+++ Loiane Groner/Segunda Lista/ex11.java
@@ -1,36 +1,32 @@
 import java.util.Scanner;
 
-public class ex10{
+public class ex11{
 
-  public static void main(String[] args){
-  
-    Scanner scan = new Scanner(System.in);
-    
-    System.out.println("Informe o salário do colaborador: ");
-    double salario = scan.nextDouble();
-    double acrescimoSalario = 0;
-    double porcem = 0;
-    
-    System.out.println("O salário do colaborador é de: " + salario);
-    
-    if(salario <= 280){
-      porcem = 0.2;
-      acrescimoSalario = (salario * porcem)/100;
-    }else if(salario > 280 && salario <= 700){
-      porcem = 0.15;
-      acrescimoSalario = (salario * porcem)/100;
-    }else if(salario > 700 && salario <= 1500){
-      porcem = 0.1;
-      acrescimoSalario = (salario * porcem)/100;
-    }else{
-      porcem = 0.05;
-      acrescimoSalario = (salario * porcem)/100;
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Informe o salário do colaborador: ");
+        double salario = scan.nextDouble();
+
+        int porcem;
+        if(salario <= 280){
+            porcem = 20;
+        }else if(salario > 280 && salario <= 700){
+            porcem = 15;
+        }else if(salario > 700 && salario <= 1500){
+            porcem = 10;
+        }else{
+            porcem = 5;
+        }
+
+        double acrescimoSalario = (salario / 100) * porcem;
+
+        System.out.println("Salário (R$): " + salario);
+        System.out.println("Percentual (%): " + porcem);
+        System.out.println("Aumento (R$): " + acrescimoSalario);
+        System.out.println("");
+        System.out.println("============ | TOTAL | ============" );
+        System.out.println("SALÁRIO REAJUSTADO: " + (salario + acrescimoSalario));
     }
-    
-    System.out.println("O valor acrescido foi de: " + acrescimoSalario);
-    System.out.println("O percentual reajustado foi de: " + porcem);
-    System.out.println("");
-    System.out.println("    ============ | TOTAL | ============" );
-    System.out.println("SALÁRIO REAJUSTADO: " + (salario + acrescimoSalario));
-  }
 }
