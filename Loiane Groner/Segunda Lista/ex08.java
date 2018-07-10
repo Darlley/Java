4fbbdd1ecc0451757877429c61194023c8ac4784
Darlley <37590954+Darlley@users.noreply.github.com>
2018-07-10 09:38:06 -0400

    CORRIGIDO ✔

--- Loiane Groner/Segunda Lista/ex08.java
+++ Loiane Groner/Segunda Lista/ex08.java
@@ -7,17 +7,15 @@ public class ex08 {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Informe o preço do primeiro produto: ");
-        int umProduto  = scan.nextInt();
-
+        double umProduto  = scan.nextDouble();
         System.out.println("Informe o preço do segundo produto: ");
-        int outroProduto  = scan.nextInt();
-
+        double outroProduto  = scan.nextDouble();
         System.out.println("Informe o preço do terceiro produto: ");
-        int maisUmProduto  = scan.nextInt();
+        double maisUmProduto  = scan.nextDouble();
 
-        if(umProduto > outroProduto && outroProduto > maisUmProduto){
+        if(umProduto => outroProduto && outroProduto => maisUmProduto){
             System.out.println("Você deve comprar o terceiro produto");
-        }else if(umProduto > outroProduto && outroProduto < maisUmProduto){
+        }else if(umProduto => outroProduto && outroProduto <= maisUmProduto){
             System.out.println("Você deve comprar o segundo produto");
         }else{
             System.out.println("Você deve comprar o primeiro produto");
