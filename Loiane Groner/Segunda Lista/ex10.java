539e028a5833531fa156d61cd7b7b601b8f491e5
Darlley <37590954+Darlley@users.noreply.github.com>
2018-07-10 09:52:38 -0400

    CORRIGIDO ✔

--- Loiane Groner/Segunda Lista/ex10.java
+++ Loiane Groner/Segunda Lista/ex10.java
@@ -1,26 +1,33 @@
 import java.util.Scanner;
 
-public class ex10{
+public class ex10 {
 
-  public static void main(String[] args){
-  
-    Scanner scan = new Scanner(System.in);
-    
-    String mensagem = '';
-    
-    System.out.println("Informe o turno em que estuda: "); 
-    System.out.println("ATENÇÃO: V para vespertino, M para Matutino e N para Noturno!");
-    String turnoInformado = scan.nextLine();
-    
-    swith(turnoInformado){
-      case "M":
-        mesagem = "Bom dia!";
-      case "V":
-        mesagem = "Boa tarde!";
-      case "N":
-        mesagem = "Boa noite!";
-      default:
-        mesagem = "Valor inválido!";
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Informe o turno em que estuda: ");
+        System.out.println("ATENÇÃO: V para vespertino, M para Matutino e N para Noturno!");
+        String turnoInformado = scan.nextLine();
+
+        String mensagem;
+
+        switch(turnoInformado) {
+            case "M":
+            case "m":
+                mensagem = "Bom dia!";
+                break;
+            case "V":
+            case "v":
+                mensagem = "Boa tarde!";
+                break;
+            case "N":
+            case "n":
+                mensagem = "Boa noite!";
+                break;
+            default:
+                mensagem = "Valor inválido!";
+
+        }
     }
-  }
 }
