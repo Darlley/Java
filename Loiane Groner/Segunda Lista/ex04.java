83c59ca32e164d2a3a1d2df36c9dfe6ecdf60eda
Darlley <37590954+Darlley@users.noreply.github.com>
2018-07-10 09:28:34 -0400

    CORRIGIDO ✔

--- Loiane Groner/Segunda Lista/ex04.java
+++ Loiane Groner/Segunda Lista/ex04.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
 
-public class ex01 {
+public class ex04 {
 
     public static void main(String[] args){
 
         Scanner scan = new Scanner(System.in);
 
-        String umaLetra;
         System.out.println("Informe uma letra[em maiuscula]:");
-        umaLetra = scan.nextLine();
+        String umaLetra = scan.nextLine();
 
         switch(umaLetra){
             case "A":
@@ -16,34 +15,15 @@ public class ex01 {
             case "I":
             case "O":
             case "U":
+            case "a":
+            case "e":
+            case "i":
+            case "o":
+            case "u":
                 System.out.println("O valor informado é uma vogal!");
                 break;
-            case "B":
-            case "C":
-            case "D":
-            case "F":
-            case "G":
-            case "H":
-            case "J":
-            case "K":
-            case "L":
-            case "M":
-            case "N":
-            case "P":
-            case "Q":
-            case "R":
-            case "S":
-            case "T":
-            case "V":
-            case "W":
-            case "X":
-            case "Y":
-            case "Z":
-                System.out.println("O valor informado é uma consoante!");
-                break;
             default:
-                System.out.println("Valor invalido!");
-                break;
+                System.out.println("O valor informado é uma consoante!");
         }
     }
 }
