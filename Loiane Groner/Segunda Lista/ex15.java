4aa152b9a515533f530dffbb7dc88d4fc14dadf9
Darlley <37590954+Darlley@users.noreply.github.com>
2018-07-11 11:07:15 -0400

    CORRIGIDO ✔

--- Loiane Groner/Segunda Lista/ex15.java
+++ Loiane Groner/Segunda Lista/ex15.java
@@ -13,14 +13,16 @@ public class ex15 {
         System.out.println("Informe o terceiro lado: ");
         int lado3  = scan.nextInt();
 
-        if(lado1 + lado2 > lado3 || lado1 + lado3 > lado2 || lado2 + lado3 > lado1){
-            System.out.printl("Forma um triângulo!");
-        }else if(lado1 == lado2 && lado2 == lado3){
-            System.out.printl("Forma um triângulo Equilatero!");
-        }else if(lado1 == lado2 || lado1 == lado3 || lado2 == lado3){
-            System.out.printl("Forma um triângulo Isósceles!");
-        }else(lado1 != lado2 && lado1 != lado3 && lado2 != lado3){
-            System.out.printl("Forma um triângulo Escaleno!");
+        if(lado1 + lado2 > lado3 && lado1 + lado3 > lado2 && lado2 + lado3 > lado1){
+            if(lado1 == lado2 && lado1 == lado3 && lado2 == lado3){
+                System.out.println("Forma um triângulo Equilatero!");
+            }else if(lado1 == lado2 && lado1 == lado3 && lado2 == lado3){
+                System.out.println("Forma um triângulo Isósceles!");
+            }else(lado1 != lado2 && lado1 != lado3 && lado2 != lado3){
+                System.out.println("Forma um triângulo Escaleno!");
+            }
+        }else if{
+            System.out.println("Não forma um triângulo!");
         }
     }
 }
