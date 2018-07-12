36650be87f28772f82c71b050882bb07762fb72a
Darlley <37590954+Darlley@users.noreply.github.com>
2018-07-12 10:39:49 -0400

    Create ex18.java

---
+++ Loiane Groner/Segunda Lista/ex18.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ex18 {
+
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Informe um número inteiro: ");
+        int numero  = scan.nextInt();
+        
+        if(numero % 2 == 0){
+              System.out.println("O número é par!")
+        }else{
+              System.out.println("O número é impar")
+        }
+        
+        if(numero > 0){
+          System.out.println("O número é positivo!")
+        }else{
+          System.out.println("O número é negativo!")
+        }
+    }
+}
