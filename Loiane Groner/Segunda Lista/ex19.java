ddfcc6f82dc01463631db1cc2ec1b0002c00c48f
Darlley <37590954+Darlley@users.noreply.github.com>
2018-07-12 10:51:28 -0400

    Create ex19.java

---
+++ Loiane Groner/Segunda Lista/ex19.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class ex18 {
+
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Informe um número: ");
+        int numero1  = scan.nextInt();
+        
+        System.out.println("Informe outro número: ");
+        int numero2  = scan.nextInt();
+        
+        System.out.println("Qual operação deseja realizar?: ");
+        String operador  = scan.nextInt();
+        
+        double operacao = 0; 
+        
+        switch(operador){
+          case "+":
+            operacao = numero1 + numero2;
+            break;
+          case "-":
+            operacao = numero1 - numero2;
+            break;
+          case "*":
+            operacao = numero1 * numero2;
+            break;
+          case "/":
+            operacao = numero1 / numero2;
+            break;
+          defaut: 
+            System.out.println("Operador inválido");
+        }
+        
+        System.out.println("Resultado: " + operacao);
+         
+        if(operacao % 2 == 0){
+              System.out.println("O número resultante é par!")
+        }else{
+              System.out.println("O número resultante é impar")
+        }
+        
+        if(operacao > 0){
+            System.out.println("O número resultante é positivo!")
+        }else{
+          System.out.println("O número resultante é negativo!")
+        }
+    }
+}
+
+
+
+
