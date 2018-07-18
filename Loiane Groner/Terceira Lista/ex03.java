fe93ea7daee2da13a04e7959009c0e8a784b2290
Darlley <37590954+Darlley@users.noreply.github.com>
2018-07-18 15:25:56 -0400

    CORRIGIDO ✔

--- Loiane Groner/Terceira Lista/ex03.java
+++ Loiane Groner/Terceira Lista/ex03.java
@@ -5,58 +5,90 @@ public class ex03 {
     public static void main(String[] args){
 
         Scanner scan = new Scanner(System.in);
-
+        
+        boolean infoValida = false;
+        String nome, sexo, estadoCivil;
+        int idade;
+        double salario;
+        
+        
         // NOME
-        System.out.println("Informe um nome(até 3 caracteres): ");
-        String nome = scan.nextLine();
-
-        if(nome.length() > 3){
-            System.out.println("Tamanho do nome inválido!");
-            System.out.println("Informe um nome(até 3 caracteres): ");
-            nome = scan.nextLine();
-        }
-
+        
+        do{
+            System.out.println("Informe um nome: ");
+            nome = scan.next();
+            if(nome.length() > 3){
+                infoValida = true;
+            } else {
+                System.out.println("Tamanho do nome inválido!");
+                System.out.println("Informe um nome(até 3 caracteres): ");
+            }
+        }while(!infoValida)
+        infoValida = false;
+        
+        
         // IDADE
-        System.out.println("Informe uma idade(até 150): ");
-        int idade = scan.nextInt();
-
-        if(idade > 150){
-            System.out.println("Tamanho idade inválido!");
-            System.out.println("Informe uma idade(até 150): ");
+        
+        do{
+            System.out.println("Informe a idade: ");
             idade = scan.nextInt();
-        }
+            
+            if(idade >= 0 && idade <= 150){
+                infoValida = true;
+            } else {
+                System.out.println("Idade precisa ser entre 0 e 150!");
+            }   
+        }while(!infoValida)
 
-
+            
         // SALÁRIO
-        System.out.println("Informe um salário(Maior que 0): ");
-        double salario = scan.nextDouble();
-
-        if(salario <= 0){
-            System.out.println("Quantidade salário inválido!");
-            System.out.println("Informe um salário(Maior que 0): ");
+            
+        do{
+            System.out.println("Informe o salário: ");
             salario = scan.nextDouble();
-        }
+            
+            if(salario > 0){
+                infoValida = true;
+            } else {
+                System.out.println("Salário precisa ser maior que 0!");
+            }   
+        }while(!infoValida)
+        infoValida = false;
 
 
         //sexo
-        System.out.println("Informe um sexo(M, F): ");
-        String sexo = scan.nextLine();
+        do{
+            System.out.println("Informe o sexo: ");
+            sexo = scan.next();
+            
+            if(sexo.equalsIgnoreCase("f") || sexo.equalsIgnoreCase("m")){
+                infoValida = true;
+            } else {
+                System.out.println("Tipo SEXO inválido!");
+            }   
+        }while(!infoValida)
+        infoValida = false;
 
-        if(sexo != "M" && sexo != "F"){
-            System.out.println("Tipo sexo inválido!");
-            System.out.println("Informe um sexo(M, F): ");
-            sexo = scan.nextLine();
-        }
-
+    
         //Estado Civíl
-        System.out.println("Informe um estado cívil[S, C, V ou D]: ");
-        String estadoCiv = scan.nextLine();
-
-        if(estadoCiv != "S" && estadoCiv != "C" && estadoCiv != "V" && estadoCiv != "D"){
-            System.out.println("Tipo estado civíl inválido!");
-            System.out.println("Informe um estado cívil[S, C, V ou D]: ");
-            estadoCiv = scan.nextLine();
-        }
-
+        do{
+            System.out.println("Informe o Estado Civíl: ");
+            estadoCivil = scan.next();
+            
+            if(estadoCivil.equalsIgnoreCase("s") || sexo.equalsIgnoreCase("c" || estadoCivil.equalsIgnoreCase("v") || sexo.equalsIgnoreCase("d")){
+                infoValida = true;
+            } else {
+                System.out.println("Tipo ESTADO CIVIL inválido!");
+            }   
+        }while(!infoValida)
+        infoValida = false;
+        
+               
+        // SAÍDA
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Salário: " + salario);
+        System.out.println("Sexo: " + sexo);
+        System.out.println("Estado Civil: " + estadoCivil);
     }
 }
