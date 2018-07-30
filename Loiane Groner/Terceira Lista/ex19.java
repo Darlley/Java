351dd2c699b8438b2be95f99bc5d81fdbeeba363
Darlley <37590954+Darlley@users.noreply.github.com>
2018-07-30 14:40:36 -0400

    Update ex19.java

--- Loiane Groner/Terceira Lista/ex19.java
+++ Loiane Groner/Terceira Lista/ex19.java
@@ -1,28 +1,25 @@
 import java.util.Scanner;
 
-public class ex19{
+public class ex01{
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        
-        int continue = 1;
+
         double media = 0;
-        
-        while(continue == 1){
-          System.out.print("Digite uma nota: ");
-          int nota = scan.nextInt();
-          
-          System.out.print("[1] para CONTINUAR!");
-          System.out.print("[2] para PARAR!");
-          int continue = scan.nextInt();
-          
-          for(int i = 0; continue == 1; i++){
-            nota = nota / i;
-          }
-          
+        int resp = 1;
+        int i = 0;
+
+        while(resp == 1){
+            System.out.println("Informe uma nota: ");
+            double nota = scan.nextDouble();
+            System.out.println("Deseja continuar? [1] - Sim/[2] - Não");
+            resp = scan.nextInt();
+
+            nota += nota;
+            i++;
+            media = nota/i;
         }
-        
-        System.out.println("A média é: " + nota);
+        System.out.println("Media: " + media);
     }
 }
