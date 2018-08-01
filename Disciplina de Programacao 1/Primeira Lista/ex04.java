1f145afa509d421b26d8f48cf33e18a0d4376570
Darlley <37590954+Darlley@users.noreply.github.com>
2018-07-31 22:12:37 -0400

    CORRIGIDO ✔

--- Disciplina de Programacao 1/Primeira Lista/ex04.java
+++ Disciplina de Programacao 1/Primeira Lista/ex04.java
@@ -1,22 +1,13 @@
-import java.util.Scanner;
-
 public class ex04{
+	public static void main(String args[]){
+	
+		double anos = Input.readDouble("A quantos anos você fuma? ");
+		double cigarros = Input.readDouble("Quantos cigarros você fuma por dia? ");
+		double carteira = Input.readDouble("Quantos esta a carteira de cigarro? ");
+		double total;
 
-    public static void main(String[] args){
-
-        Scanner scan = new Scanner(System.in);
-
-        double total;
-
-        System.out.println("Informe o número de anos que fuma: ");
-        double anos = scan.nextDouble();
-        System.out.println("Informe o número de cigarros fumados por dia: ");
-        double numCigarros = scan.nextDouble();
-        System.out.println("Informe o valor da carteira de cigarro: ");
-        double valor = scan.nextDouble();
-
-        total = valor * ((365 * anos) * numCigarros) ;
-
-        System.out.println("Você gastou até agora " + total + " em cigarros.");
-    }
-}
+		total = (((anos * 365) * cigarros) / 20) * carteira;
+		
+		System.out.println(total);
+	}		
+} 
