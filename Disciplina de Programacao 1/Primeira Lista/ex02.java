5f98861ee39f394daf7385a8686f4125eeead127
Darlley <37590954+Darlley@users.noreply.github.com>
2018-07-31 22:11:26 -0400

    CORRIGIDO ✔

--- Disciplina de Programacao 1/Primeira Lista/ex02.java
+++ Disciplina de Programacao 1/Primeira Lista/ex02.java
@@ -1,23 +1,19 @@
-import java.util.Scanner;
-
 public class ex02{
-
-    public static void main(String[] args){
-
-        Scanner scan = new Scanner(System.in);
-
-        int totalSegundos;
-
-        System.out.println("Informe o horario detalhadamente abaixo:");
-        System.out.println("Horas: ");
-        int horas = scan.nextInt();
-        System.out.println("Minutos: ");
-        int minutos = scan.nextInt();
-        System.out.println("Segundos: ");
-        int segundos = scan.nextInt();
-
-        totalSegundos = ((60 * minutos) * (60 * horas)) + segundos;
-
-        System.out.println("O horário " + horas + ":" + minutos + ":" + segundos + " tem " + totalSegundos + " segundos.");
-    }
-}
+	public static void main(String args[]){
+	
+		int horas;
+		int minutos;
+		int segundos;
+		int totalSegundos;
+		
+		horas = Input.readInt("Informe as horas: ");
+		minutos = Input.readInt("Informe os minutos: ");
+		segundos = Input.readInt("Informe os segundos: ");
+	
+		totalSegundos = (horas * 60) * 60;
+		totalSegundos = totalSegundos + (minutos * 60);
+		totalSegundos = totalSegundos + segundos;
+		
+		System.out.println(horas + ":" + minutos + ":" + segundos + " tem " + totalSegundos + " segundos.");		
+		
+	}		
