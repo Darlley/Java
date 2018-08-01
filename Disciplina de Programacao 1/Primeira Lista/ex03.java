10114aeb5ef22e055b49009ee19c32a498fb669b
Darlley <37590954+Darlley@users.noreply.github.com>
2018-07-31 22:12:08 -0400

    CORRIGIDO ✔

--- Disciplina de Programacao 1/Primeira Lista/ex03.java
+++ Disciplina de Programacao 1/Primeira Lista/ex03.java
@@ -1,22 +1,13 @@
-import java.util.Scanner;
-
 public class ex03{
+	public static void main(String args[]){
+	
+		double custo = Input.readDouble("Valor de custo: ");
+		double imposto = Input.readDouble("Valor de imposto: ");
+		double lucro = Input.readDouble("Valor de lucro: ");
+		double total;
 
-    public static void main(String[] args){
-
-        Scanner scan = new Scanner(System.in);
-
-        double total;
-
-        System.out.println("Informe o custo do veículo:");
-        double custo = scan.nextDouble();
-        System.out.println("Informe a porcentagem de impostos: ");
-        double porcem = scan.nextDouble();
-        System.out.println("Informe a porcentagem do lucro: ");
-        double lucro = scan.nextDouble();
-
-        total = custo + ((custo/100) * porcem) + ((custo/100)* lucro);
-
-        System.out.println(custo + " + " + porcem + " + " + lucro + " = " + total);
-    }
+		total = custo + ((custo/100)*imposto) + ((custo/100)*lucro);
+		
+		System.out.println("R$" + custo + " + R$" + imposto + " + R$" + lucro + " = " + total);
+	}		
 }
