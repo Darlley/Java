26090623d83189c8f24d322c3a268e6144ec8414
Darlley <37590954+Darlley@users.noreply.github.com>
2018-08-04 12:07:30 -0400

    Update ex07.java

--- Disciplina de Programacao 1/Primeira Lista/ex07.java
+++ Disciplina de Programacao 1/Primeira Lista/ex07.java
@@ -1,23 +1,17 @@
-import java.util.Scanner;
-
 public class ex07{
 
     public static void main(String[] args){
 
-        Scanner scan = new Scanner(System.in);
-
-        double screen;
         double jose = 1.50;
         double pedro = 1.10;
 
-        System.out.println("Informe a quantidade de anos: ");
-        double ano = scan.nextDouble();
+        double ano = scan.nextDouble("Informe a quantidade de anos: ");
         
         jose += (2 *  ano);
         pedro += (3 * ano);
         
         System.out.println("Em " + ano + " anos...");
         System.out.println("José terá: " + jose + "cm.");
-        System.out.println("Pedro terá: " + jose + "cm.");
+        System.out.println("Pedro terá: " + pedro + "cm.");
     }
 }
