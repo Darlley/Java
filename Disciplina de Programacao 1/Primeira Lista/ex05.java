ef77af54d74e67b91904eae8b9dcd0f527324d9d
Darlley <37590954+Darlley@users.noreply.github.com>
2018-08-04 12:04:45 -0400

    Update ex05.java

--- Disciplina de Programacao 1/Primeira Lista/ex05.java
+++ Disciplina de Programacao 1/Primeira Lista/ex05.java
@@ -1,16 +1,10 @@
-import java.util.Scanner;
-
 public class ex05{
 
     public static void main(String[] args){
 
-        Scanner scan = new Scanner(System.in);
-
         double cobre;
         double zinco;
-
-        System.out.println("Qual a quantida de latão você  quer: ");
-        double qtd = scan.nextDouble();
+        double qtd = Input.readDouble("Qual a quantida de latão você  quer: ");
 
         cobre = (qtd/100) * 70;
         zinco = (qtd/100) * 30;
