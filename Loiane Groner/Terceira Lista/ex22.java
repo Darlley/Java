eaa11ed6096c915808187c352dbb4074b75a06da
Darlley <37590954+Darlley@users.noreply.github.com>
2018-08-05 14:11:46 -0400

    Create ex22.java

---
+++ Loiane Groner/Terceira Lista/ex22.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ex22{
+
+  public static void main(String[] args) {
+  
+      int continue = 1;
+      dpuble continue = 0;
+      double total;
+
+      for(double i = 0; continue == 1; i++){
+        while(quantidade => i){
+          System.out.print("Informe a quantidade de CDs: ")
+          quantidade = scan.nextDouble();
+
+          System.out.print("Infore o preço do primeiro CD: ")
+          total = scan.nextDouble();
+
+          System.out.print("Tem mais turmas? [1] - Sim [2] - Não ")
+          continue = scan.nextInt();
+
+          total += quantidade;
+          total += (total / i);
+
+        }
+      }
+      
+      System.out.print("A média é " + total);
+}
