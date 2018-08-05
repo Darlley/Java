3b33ce218e591068f690e00711bf2bccecea37ab
Darlley <37590954+Darlley@users.noreply.github.com>
2018-08-05 00:55:41 -0400

    Create ex13.java

---
+++ Disciplina de Programacao 1/Primeira Lista/ex13.java
@@ -0,0 +1,9 @@
+public clas ex13{
+  public static void main(Strin[] args){
+    double temp = InputDouble("Informe a temperatura Célcius: ");
+
+    temp = (temp * 32) / 1,8;
+
+    System.out.print("A temperatura convertida para Fahrenheit: " + temp);
+  }
+}
