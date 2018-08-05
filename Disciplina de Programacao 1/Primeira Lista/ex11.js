aa5601e0df4443f659465299437d20990249e164
Darlley <37590954+Darlley@users.noreply.github.com>
2018-08-05 00:25:13 -0400

    Create ex11.js

---
+++ Disciplina de Programacao 1/Primeira Lista/ex11.js
@@ -0,0 +1,11 @@
+public clas ex11{
+  public static void main(Strin[] args){
+    double saldoFinal;
+    double salarioB = InputDouble("Informe o valor do salário: ");
+    double adiantamento = InputDouble("Informe o valor do adiantamento: ");
+
+    saldoFinal = (salario bruto - (salarioB/100)*20) + adiantamento;
+
+    System.out.print("Valor a ser entregue: " + saldoFinal);
+  }
+}
