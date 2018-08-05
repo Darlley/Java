26ede525518d9694a30b59ada3684e282e559093
Darlley <37590954+Darlley@users.noreply.github.com>
2018-08-05 00:54:24 -0400

    Create ex12.java

---
+++ Disciplina de Programacao 1/Primeira Lista/ex12.java
@@ -0,0 +1,9 @@
+public clas ex11{
+  public static void main(Strin[] args){
+    double temp = InputDouble("Informe a temperatura: ");
+
+    temp = temp * 1,8 + 32;
+
+    System.out.print("A temperatura convertida é: " + temp);
+  }
+}
