3c287d8e008ab3555aa98a7a1fd3676a2576a91d
Darlley <37590954+Darlley@users.noreply.github.com>
2018-08-11 19:45:22 -0400

    CORRIGIDO ✔

---
+++ Loiane Groner/Terceira Lista/ex24.java
@@ -0,0 +1,11 @@
+public class ex23{
+
+    public static void main(String[] args) {
+    System.out.println("Preço do Pão: R$0.18");
+      System.out.println(" - Tabela de Preços -");
+      
+      for(int i=1; i<=50; i++){
+        System.out.println(i + " - R$" + (0.18 * i);
+      }
+    }
+}
