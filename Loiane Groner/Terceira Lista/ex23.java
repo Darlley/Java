126a79304a10d67cf905a6dc15a4236066c4be10
Darlley <37590954+Darlley@users.noreply.github.com>
2018-08-11 19:43:35 -0400

    CORRIGIDO ✔

---
+++ Loiane Groner/Terceira Lista/ex23.java
@@ -0,0 +1,10 @@
+public class ex23{
+
+    public static void main(String[] args) {
+      System.out.println(" - Tabela de Preços -");
+      
+      for(int i=1; i<=50; i++){
+        System.out.println(i + " - R$" + (1.99 * i);
+      }
+    }
+}
