c9e5cdf01eec2402ef8b9673f1dc410a6518a67b
Darlley <37590954+Darlley@users.noreply.github.com>
2018-08-12 20:45:28 -0300

    CORRIGIDO ✔

---
+++ Loiane Groner/Terceira Lista/ex26.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ex17{
+
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Informe um número: ");
+        int num = scan.nextInt();
+
+        System.out.println("Fatorial de " + num);
+        System.out.print(num + "! = ");
+
+        int fatorial = 1;
+        for(int i=num; i>1; i--){
+            fatorial *= i;
+            System.out.print(i + " x ");
+        }
+        System.out.print("1 = " + fatorial);
+    }
+}
