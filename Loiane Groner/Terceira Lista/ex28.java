47463da64f3cc23ac4c42a8d9d500c68c670aac3
Darlley <37590954+Darlley@users.noreply.github.com>
2018-08-12 20:56:13 -0300

    CORRIGIDO ✔

---
+++ Loiane Groner/Terceira Lista/ex28.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ex28{
+
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        boolean primo = true;
+
+        System.out.println("Informe um número: ");
+        int number = scan.nextInt();
+
+        for(int i = 2; i < number; i++){
+            if(number % i == 0){
+                System.out.println("Não é primo!");
+                primo = false;
+                break;
+            }
+        }
+
+        if(primo){
+            System.out.print("O número digitado é Primo!");
+        }
+    }
+}
