38f178f92f00c5910a0b95e262645a46a6b75c82
Darlley <37590954+Darlley@users.noreply.github.com>
2018-08-14 14:47:08 -0400

    Create ex01.java

---
+++ Curso em Video/POO/CLASSES/ex01.java
@@ -0,0 +1,21 @@
+package aula02;
+
+public class Livro{
+
+  Strin autor;
+  String nome;
+  String ISBN;
+  int paginas;
+  double preco;
+  boolean vendido;
+  
+  void abrir(){
+  
+  }
+  void fechar(){
+  
+  }
+  void emprestar(){
+  
+  }
+}
