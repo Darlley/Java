5cb8ff55955c143dcfe52f06c0a1558a9c298c33
Darlley <37590954+Darlley@users.noreply.github.com>
2018-08-14 22:15:29 -0400

    Update ex01.java

--- Disciplina de Programacao 1/Terceira Lista/ex01.java
+++ Disciplina de Programacao 1/Terceira Lista/ex01.java
@@ -1,5 +1,33 @@
-public clas ex01{
-  public static void main(String[] args){
-
-  }
+public class ex01{
+	public static void main(String args[]){
+	
+		double nota = 0, media = 0, maior = 0, menor = 0;
+		int i = 0, j = 1;
+		
+		System.out.println("====== INDICE =====");	
+		System.out.println("0 - Parar");
+		System.out.println("1 - Continuar");
+		System.out.println("===================\n");
+		
+		
+		while(j == 1){
+	
+			nota = Input.readDouble("Informe a nota " + (++i) + ": ");
+			
+			if(media < nota){
+				menor = nota;
+			}else{
+				maior = nota;
+			}
+			media = nota;
+			j = Input.readInt("Deseja continuar[0/1]? ");
+		}
+		
+		media = (menor + maior) / i;
+		
+		System.out.println("A media é: " + media);
+		System.out.println("O menor é: " + menor);
+		System.out.println("O maior é: " + maior);
+		
+	}
 }
