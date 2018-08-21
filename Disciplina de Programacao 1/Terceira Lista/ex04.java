f2d212ace6e5c3a561fdae77cf4703bf64d5a733
Darlley <37590954+Darlley@users.noreply.github.com>
2018-08-21 15:41:30 -0400

    Create ex04.java

---
+++ Disciplina de Programacao 1/Terceira Lista/ex04.java
@@ -0,0 +1,20 @@
+public class ex01{
+	public static void main(String[] args){
+		int n = Input.readInt("Informe um número: ");
+
+		int primeiro = 1;
+        int segundo = 1;
+        int proximo;
+
+        System.out.println(primeiro);
+        System.out.println(segundo);
+
+        for(int i = 3; i <= n; i++){
+            proximo = primeiro + segundo;
+            primeiro = segundo;
+            segundo = proximo;
+
+            System.out.println(proximo);
+        }
+	}
+}
