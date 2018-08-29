ad1427b98fca621a5ff0162649c44a87f5fd002f
Darlley <37590954+Darlley@users.noreply.github.com>
2018-08-28 20:40:03 -0400

    Create ex04.java

---
+++ Disciplina de Programacao 1/Sexta Lista/ex04.java
@@ -0,0 +1,22 @@
+ public class exemplo6{
+	public static void main(String args[]){
+	
+		int i=0, vetor [] = new int[10];
+		
+		primo = 100;
+		do{
+			vetor[i] = primo++;
+			System.out.print(primo + "\n");
+			primo++;
+			i++;
+		}while(primo<110);
+		
+
+	}
+}
+
+/*
+Escrever um algoritmo que gera os 10 primeiros
+números primos acima de 100 e os armazena em um
+vetor de X escrevendo, no final, o vetor X.
+*/
