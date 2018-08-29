0fad7032852cffecf8dbec5ceeef18d177fb2fe2
Darlley <37590954+Darlley@users.noreply.github.com>
2018-08-28 20:14:06 -0400

    Create ex01.java

---
+++ Disciplina de Programacao 1/Sexta Lista/ex01.java
@@ -0,0 +1,31 @@
+ public class ex01{
+	public static void main(String args[]){
+	
+		int i = 0, x, indice, vetor [] = new int[20]; 
+		boolean naoachou;
+		
+		do{
+			vetor[i] = Input.readInt("Valor "+ (i+1) +": ");
+			i++;
+		}while(i<20);
+
+		x = Input.readInt("Valor de X: ");
+	
+		i=0;
+		indice = 0;
+		
+		naoachou = true;
+		while(i<20 && naoachou==true){
+			if(vetor[i] == x){
+				indice = i;
+				naoachou = false;
+			}
+			i++;	
+		}
+		
+		if(indice!=0)
+			System.out.println("O valor "+ x +" esta no indice "+ indice + "!");
+		else
+			System.out.println("O valor "+x+" nao esta no vetor!");
+	}
+}
