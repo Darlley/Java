18f3d2915f44d3d539943c1f4c9455ee7279a457
Darlley <37590954+Darlley@users.noreply.github.com>
2018-08-28 20:32:05 -0400

    Create ex03.java

---
+++ Disciplina de Programacao 1/Sexta Lista/ex03.java
@@ -0,0 +1,29 @@
+ public class exemplo6{
+	public static void main(String args[]){
+	
+		int i = 0, vetor1 [] = new int[10], vetor2 [] = new int[10], vetor3 [] = new int[10];
+		
+		System.out.print("=== VETOR 1 ===\n");
+		
+		do{
+			vetor1[i] = Input.readInt("Valor "+ (i+1) +": ");
+			i++;
+		}while(i<10);
+		i=0;
+		
+		System.out.print("\n=== VETOR 2 ===\n");
+		
+		do{
+			vetor2[i] = Input.readInt("Valor "+ (i+1) +": ");
+			i++;
+		}while(i<10);
+		
+		System.out.print("\n=== RESULTADO ===\n");
+	
+		for(i=0; i<10; i++){
+			vetor3[i] = (vetor1[i] * vetor2[i]);
+			System.out.print("\nIndice " + i + " = " + vetor3[i] + "\n");
+		}
+
+	}
+}
