e627d1045230ba2245a327e0f182a4fd70331364
Darlley <37590954+Darlley@users.noreply.github.com>
2018-09-03 15:11:09 -0400

    ✖

---
+++ Disciplina de Programacao 1/Quinta Lista/ex05.java
@@ -0,0 +1,20 @@
+public class ex01{
+    public static void main(String[] args){
+    
+    
+    
+    }
+}
+/*
+Numa universidade cada aluno possui os seguintes dados:
+- Renda pessoal;
+- Renda familiar;
+- Total gasto com alimentação;
+- Total gasto com outras despesas;
+Criar um programa que imprima a porcentagem dos alunos que
+gasta acima de R$ 200,00 com outras despesas, o número de
+alunos com renda pessoal maior que renda familiar e a
+porcentagem gasta com alimentação e outras despesas em relação
+às rendas pessoal e familiar. O algoritmo acaba quando se digita 0
+para a renda pessoal.
+*/
