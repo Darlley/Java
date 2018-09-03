a04354b8b658109669676ff23e555cd21128765e
Darlley <37590954+Darlley@users.noreply.github.com>
2018-09-03 15:09:48 -0400

    ✖

---
+++ Disciplina de Programacao 1/Quinta Lista/ex04.java
@@ -0,0 +1,19 @@
+public class ex01{
+    public static void main(String[] args){
+      
+    }
+}
+
+/*
+Em uma eleição presidencial, existem quatro candidatos. Os
+votos são informados através de código. Os dados utilizados para o
+levantamento obedecem à seguinte codificação:
+- 1, 2, 3 e 4 = voto para os respectivos candidatos;
+- 5 voto nulo;
+- 6 voto em branco;
+Elaborar um programa que calcule e imprima:
+- O total de votos para cada candidato;
+- O total de votos nulos;
+- O total de votos em branco;
+- O percentual dos votos em branco e nulos sobre o total
+*/
