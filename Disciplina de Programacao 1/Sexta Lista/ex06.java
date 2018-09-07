4cf3a1c1a7054aac78a14afc2a018c02eb7e68b2
Darlley <37590954+Darlley@users.noreply.github.com>
2018-09-07 15:52:01 -0400

    Create ex06.java

---
+++ Disciplina de Programacao 1/Sexta Lista/ex06.java
@@ -0,0 +1,10 @@
+/*
+Uma locadora de vídeos tem guardada, em um
+vetor de 50 posições, a quantidade de filmes retirados
+por seus clientes (50) durante o ano de 2011. Agora,
+esta locadora está fazendo uma promoção e, para 
+cada 10 filmes retirados, o cliente tem direito a uma
+locação grátis. Faça um algoritmo que crie um outro
+vetor contendo a quantidade de locações gratuitas a
+que cada cliente tem direito.
+*/
