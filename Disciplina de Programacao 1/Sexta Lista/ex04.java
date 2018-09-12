1b2eba2e9d7f9b682c284618b65c0cf26c6c509f
Darlley <37590954+Darlley@users.noreply.github.com>
2018-09-11 21:18:54 -0400

    ✔

--- Disciplina de Programacao 1/Sexta Lista/ex04.java
+++ Disciplina de Programacao 1/Sexta Lista/ex04.java
@@ -1,21 +1,11 @@
-/*
-Escrever um algoritmo que gera os 10 primeiros
-números primos acima de 100 e os armazena em um
-vetor de X escrevendo, no final, o vetor X.
-*/
-
 public class ex04{
 	public static void main(String args[]){
-		int i=0, vetor [] = new int[10];
+		int primo = 100, i=0, vetor [] = new int[10];
 		
-		primo = 100;
 		do{
 			vetor[i] = primo++;
 			System.out.print(primo + "\n");
-			primo++;
 			i++;
 		}while(primo<110);
-		
-		vetor[]
 	}
 }
