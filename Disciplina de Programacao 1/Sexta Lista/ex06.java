884d63e527a18f79e2015c83be4da21f4a2040ef
Darlley <37590954+Darlley@users.noreply.github.com>
2018-09-11 22:00:48 -0400

    Update ex06.java

--- Disciplina de Programacao 1/Sexta Lista/ex06.java
+++ Disciplina de Programacao 1/Sexta Lista/ex06.java
@@ -1,10 +1,24 @@
-/*
-Uma locadora de vídeos tem guardada, em um
-vetor de 50 posições, a quantidade de filmes retirados
-por seus clientes (50) durante o ano de 2011. Agora,
-esta locadora está fazendo uma promoção e, para 
-cada 10 filmes retirados, o cliente tem direito a uma
-locação grátis. Faça um algoritmo que crie um outro
-vetor contendo a quantidade de locações gratuitas a
-que cada cliente tem direito.
-*/
+public class ex06{
+	public static void main(String args[]){
+		int retirar, gratuito, vetor1[] = new int[50], vetor2[] = new int[50], j=0;
+    
+		for(int i=1; i<=50; i++){
+			vetor1[i] = i;
+		}
+		
+		retirar = Input.readInt("Quantidade: ");
+		
+		for(int i=0; i<quatidade; i++){
+			vetor2[i] = vetor1[i];
+			if(vetor[i] % 10 == 0){
+				gratuito += j;
+				j++;
+			}
+		}
+		
+		int vetor3[] = new int[gratuito];
+		if(vetor3){
+		  
+		}
+	}
+}
