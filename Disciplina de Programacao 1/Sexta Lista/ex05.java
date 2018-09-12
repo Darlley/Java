ed43baa35bbd087ea41877669004335e3412c693
Darlley <37590954+Darlley@users.noreply.github.com>
2018-09-11 20:20:05 -0400

    Update ex05.java

--- Disciplina de Programacao 1/Sexta Lista/ex05.java
+++ Disciplina de Programacao 1/Sexta Lista/ex05.java
@@ -1,13 +1,16 @@
 public class ex05{
     public static void main(String args[]){
-        int acumulador = 0, vetorA [] = new int[50], vetorB [] = new int[50], i = 0, j = 49;
+        int acumulador = 0, vetorA [] = new int[5], vetorB [] = new int[5], i = 0, j = 4;
+        
+        for(i=0; i<5; i++){
+            vetor[i] = Input.readInt("Valor: ");
+        }
 
-        while(vetorA[i] == vetorB[j]){
+        while(i <= 5 && j != 0){
             acumulador = vetorA[i] - vetorB[j];
             i++;
             j--;
         }
         System.out.println(acumulador);
-
     }
 }
