d949e29dc119dff541e1f498183ffaee359154d3
Darlley <37590954+Darlley@users.noreply.github.com>
2018-09-15 18:59:46 -0400

    Update ex02.java

--- Loiane Groner/Quarta Lista/ex02.java
+++ Loiane Groner/Quarta Lista/ex02.java
@@ -1,5 +1,18 @@
-public class ex01{
+public class ex02{
     public static void main(String[] args){
+        int vetorA[] = new int[8], vetorB[] = new int[vetorA.length];
+        vetorA[0] = 1;
+        vetorA[1] = 2;
+        vetorA[2] = 3;
+        vetorA[3] = 4;
+        vetorA[4] = 5;
+        vetorA[5] = 6;
+        vetorA[6] = 7;
+        vetorA[7] = 8;
 
+        for(int i=0; i <= vetorA.length; ++i){
+            vetorB[i] = vetorA[i]*2;
+            System.out.println("Valor adicionado no índice " + i + ": " + vetorB[i]);
+        }
     }
 }
