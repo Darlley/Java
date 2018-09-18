8230cd06ec20d6bcf276a5a9fb47dda5c258635f
Darlley <37590954+Darlley@users.noreply.github.com>
2018-09-18 15:16:58 -0400

    ✔

--- Loiane Groner/Quarta Lista/ex03.java
+++ Loiane Groner/Quarta Lista/ex03.java
@@ -1,21 +1,14 @@
+import java.util.Scanner;
 public class ex03{
     public static void main(String[] args){
+        
+        Scanner scan = new Scanner(System.in);
         int vetorA[] = new int[15], vetorB[] = new int[vetorA.length];
-        vetorA[0] = 1;
-        vetorA[1] = 2;
-        vetorA[2] = 3;
-        vetorA[3] = 4;
-        vetorA[4] = 5;
-        vetorA[5] = 6;
-        vetorA[6] = 7;
-        vetorA[7] = 8;
-        vetorA[8] = 9;
-        vetorA[9] = 10;
-        vetorA[10] = 11;
-        vetorA[11] = 12;
-        vetorA[12] = 13;
-        vetorA[13] = 14;
-        vetorA[14] = 15;
+        
+        for(int i=0; i<=vetorA.length; i++){
+            System.out.print("Informe o valor " + (i+1));
+            vetorA[i] = scan.nextInt(); 
+        }
 
         for(int i=0; i <= vetorA.length; ++i){
             vetorB[i] = vetorA[i]*vetorA[i];
