95bf9736e236e40959a0896fe9454f0c5bb0add4
Darlley <37590954+Darlley@users.noreply.github.com>
2018-09-18 15:34:48 -0400

    Create ex09.java

---
+++ Loiane Groner/Quarta Lista/ex09.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class ex09{
+    public static void main(String[] args){
+        
+        Scanner scan = new Scanner(System.in);
+        int vetorA[] = new int[10];
+        int vetorB[] = new int[vetorA.length];
+        int vetorC[] = new int[vetorA.length];
+        
+        System.out.print("VETOR A:");
+        for(int i=0; i<vetorA.length; i++){
+            System.out.print(i + "º valor: ");
+            vetorA[i] = scan.nextInt();
+        }
+        
+        System.out.print("VETOR B:");
+        for(int i=0; i<vetorA.length; i++){
+            System.out.print(i + "º valor: ");
+            vetorB[i] = scan.nextInt();
+            vetorC[i] = vetorA[i] * vetorB[i];
+        }
+
+        System.out.print("RESULTADO: ");
+        for(int i=0; i <= vetorC.length; ++i){
+            System.out.println("Valor adicionado no índice " + i + ": " + vetorC[i]);
+        }
+    }
+}
