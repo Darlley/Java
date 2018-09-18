fe02780d7d8e3779396087b3778355430566064b
Darlley <37590954+Darlley@users.noreply.github.com>
2018-09-18 15:41:04 -0400

    ✔

--- Loiane Groner/Quarta Lista/ex10.java
+++ Loiane Groner/Quarta Lista/ex10.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
-public class ex9{
+public class ex10{
     public static void main(String[] args){
         
         Scanner scan = new Scanner(System.in);
         int vetorA[] = new int[10];
         int vetorB[] = new int[vetorA.length];
         
-        for(int i=0; i<vetorA.length; i++){
-            System.out.print(i + "º valor: ");
+        for(int i=0; i<=vetorA.length; i++){
+            System.out.print((i+1) + "º valor: ");
             vetorA[i] = scan.nextInt();
             vetorB[i] = vetorA[i] % 2;
         }
         
         System.out.print("RESULTADO: ");
-        for(int i=0; i <= vetorC.length; ++i){
-            System.out.println("Valor adicionado no índice " + i + ": " + vetorC[i]);
+        for(int i=0; i <= vetorB.length; i++){
+            System.out.println("Valor adicionado no índice " + i + ": " + vetorB[i]);
         }
     }
 }
