1b8abe2003db5ab8907f5b9b65214fcb3d09f2cd
Darlley <37590954+Darlley@users.noreply.github.com>
2018-09-22 17:10:35 -0400

    Create ex13.java

---
+++ Loiane Groner/Quarta Lista/ex13.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class ex01{
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        int soma=0, vetorA[] = new int[10];
+
+        for(int i=0; i<vetorA.length; i++){
+            vetorA[i] = scan.nextInt();
+            if(vetorA[i] % 5 == 0) {
+                soma += vetorA[i];
+            }
+        }
+        System.out.print("Quantidade de pares: " + soma);
+    }
+}
