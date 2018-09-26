340c8830c2a7cab60aa2e09e3b6ba5a994d75990
Darlley <37590954+Darlley@users.noreply.github.com>
2018-09-25 20:16:25 -0400

    Update ex01.java

--- Disciplina de Programacao 1/Prova/ex01.java
+++ Disciplina de Programacao 1/Prova/ex01.java
@@ -1,28 +1,12 @@
-/*Uma empresa decidiu fazer um levantamento em relação aos
-candidatos que se apresentarem para preenchimento de vagas no
-seu quadro de funcionários. Supondo que você seja o programador
-dessa empresa, faça um programa que leia para cada candidato a
-idade, o sexo (M ou F) e a experiência no serviço (S ou N). Para
-encerrar a entrada de dados, digite S para sair. Implemente os
-seguintes relatórios:
-
-a) número de candidatos do sexo feminino (0,5);
-b) número de candidatos do sexo masculino (0,5);
-c) a idade média dos homens que já têm experiência no serviço (1,0);
-d) número de mulheres com idade inferior a 35 anos e com
-experiência no serviço (1,0);
-e) a porcentagem dos homens com mais de 45 anos entre o total dos
-homens (1,5);
-f) a menor idade entre as mulheres que já têm experiência no serviço
-*/
-
 public class ex01{
 	public static void main(String[] args){
 	
 		char sexo = 'f', experiencia = 's', fim = 'n';
-		int idade=0, idadeE=0, a=0, b=0, c=0, d=0, e=0, maior=0, menor=0;
-
-		while(fim != 's'){
+		int idade=0, idadeE=0, a=0, b=0, c=0, d=0, e=0, maior=0, menor=0, contador=1;
+		
+		while(fim != 's' && fim != 'S'){
+			System.out.println("============= CANDIDATO " + contador + "=============");
+			contador++;
 			
 			//ENTRADA DE DADOS
 			idade = Input.readInt("Idade: ");
@@ -31,30 +15,30 @@ public class ex01{
 			
 			// VERIFICAÇÕES
 			// A) e B)
-			if(sexo == 'f'){
+			if(sexo == 'f' || sexo == 'F'){
 				a++;
 			}else{
 				b++;
 			}
 			
 			// C)
-			if(experiencia == 's'){
+			if(experiencia == 's' || experiencia == 'S'){
 				c += idade;
 				idadeE++;
 			}
 			
 			// D)
-			if(sexo == 'f' && idade < 35 && experiencia == 's'){
+			if((sexo == 'f' || sexo == 'F') && idade < 35 && (experiencia == 's' || experiencia == 'S')){
 				d++;
 			}
 			
 			// E)
-			if(sexo == 'm' && idade > 45){
+			if((sexo == 'm' || sexo == 'M') && idade > 45){
 				e++;
 			}
 			
 			// F)
-			if(sexo == 'f' && experiencia == 's'){
+			if((sexo == 'f' || sexo == 'F') && (experiencia == 's' || experiencia == 'S')){
 				if(idade > maior && idade > menor){
 					maior = idade;
 				}else{
@@ -63,14 +47,36 @@ public class ex01{
 			}
 			
 			// FIM
-			fim = Input.readChar("Deseja sair? \nSIM - s | NAO - n\n");
+			fim = Input.readChar("Deseja sair? \nSIM - s | NAO - n \n");
 		}
 		
+		// GERAR RELATÓRIO
+		System.out.println("\n============= RELATORIO =============");
+		
+		// SAIDA "A"
 		System.out.println("A) Número de candidatos do sexo feminino: " + a);
+		
+		// SAIDA "B"
 		System.out.println("B) Número de candidatos do sexo masculino: " + b);
-		System.out.println("C) A idade média dos homens que já têm experiência no serviço: " + (c/idadeE));
+		
+		// SAIDA "C"
+		if(idadeE>0){
+			System.out.println("C) A idade média dos homens que já têm experiência no serviço: " + (c/idadeE));
+		}else{
+			System.out.println("C) A idade média dos homens que já têm experiência no serviço: " + 0);
+		}
+		
+		// SAIDA "D"
 		System.out.println("D) Número de mulheres com idade inferior a 35 anos e com experiência no serviço: " + d);
-		System.out.println("E) A porcentagem dos homens com mais de 45 anos entre o total dos homens: " + (100/e)*b);
+		
+		// SAIDA "E"
+		if(e>0){
+			System.out.println("E) A porcentagem dos homens com mais de 45 anos entre o total dos homens: " + (((double)100/e)*b)); // SAIDA COM ERRO
+		}else{
+			System.out.println("E) A porcentagem dos homens com mais de 45 anos entre o total dos homens: " +0);
+		}
+		
+		// SAIDA "F"
 		System.out.println("F) A menor idade entre as mulheres que já têm experiência no serviço: " + menor);
 	}
 }
