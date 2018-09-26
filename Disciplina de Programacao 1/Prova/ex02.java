bf26e2f3bee29b7b3daaadafc446b78a21bc5b1d
Darlley <37590954+Darlley@users.noreply.github.com>
2018-09-25 20:21:39 -0400

    Update ex02.java

--- Disciplina de Programacao 1/Prova/ex02.java
+++ Disciplina de Programacao 1/Prova/ex02.java
@@ -1,26 +1,3 @@
-/*
-Uma empresa de cinema armazena, semanalmente, o número de
-ingressos vendidos para cada uma das 3 salas que possui. Para tanto
-utiliza 3 vetores distintos. Cada vetor armazena a quantidade de
-ingressos vendidos na respectiva sala em cada um dos dias da
-semana: (4,0)
-Sala1: vetor de 7 posições que armazena em cada posição o valor
-dos ingressos vendidos em um determinado dia para a sala 1;
-Sala2: vetor de 7 posições que armazena em cada posição o valor
-dos ingressos vendidos em um determinado dia para a sala 2;
-Sala3: vetor de 7 posições que armazena em cada posição o valor
-dos ingressos vendidos em um determinado dia para a sala 3.
-Faça um programa em que leia os 3 vetores descritos acima, calcule
-e mostre as seguintes informações (supondo que não haja empates):
-
-a) Quantos ingressos foram vendidos em dada semana para cada
-uma das 3 salas (1,0);
-b) Qual foi a sala que vendeu mais ingressos e em que dia isto
-ocorreu (1,5);
-c) Qual foi a sala que vendeu menos ingressos e em que dia isto
-ocorreu (1,5).
-*/
-
 public class ex02{
 	public static void main(String[] args){
 		int sala1 = 0, sala2 = 0, sala3 = 0, maior = 0, menor = 0, dia = 0;
@@ -30,9 +7,10 @@ public class ex02{
 		
 		for(int i=0; i<=6; i++){
 			// ENTRADA DE DADOS
-			vetor1[i] = Input.readInt("Informe a quantidade de ingressos vendidos no dia " + (i+1) + " para a sala 1: ");
-			vetor2[i] = Input.readInt("Informe a quantidade de ingressos vendidos no dia " + (i+1) + " para a sala 2: ");
-			vetor3[i] = Input.readInt("Informe a quantidade de ingressos vendidos no dia " + (i+1) + " para a sala 3: ");
+			System.out.println("DIA " + (i+1));
+			vetor1[i] = Input.readInt("Informe a quantidade de ingressos vendidos para a sala 1: ");
+			vetor2[i] = Input.readInt("Informe a quantidade de ingressos vendidos para a sala 2: ");
+			vetor3[i] = Input.readInt("Informe a quantidade de ingressos vendidos para a sala 3: ");
 			
 			// A)
 			sala1 += vetor1[i];
