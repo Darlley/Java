760b81b2223239fa310c9a511c6bbacf58d25a43
Darlley <37590954+Darlley@users.noreply.github.com>
2018-09-30 19:37:23 -0400

    ✔

--- Loiane Groner/Quarta Lista/ex15.java
+++ Loiane Groner/Quarta Lista/ex15.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
-public class ex01{
+public class ex15{
     public static void main(String[] args){
 
         Scanner scan = new Scanner(System.in);
-        int par=0, impar=0, soma=0, vetorA[] = new int[10];
+        int par=0, impar=0, vetorA[] = new int[10];
 
         for(int i=0; i<vetorA.length; i++){
+            System.out.print("Informe um valor: ");
             vetorA[i] = scan.nextInt();
+            
             if(vetorA[i] % 2 == 0) {
                 par++;
             }else{
                 impar++;
             }
         }
-        System.out.print("Percentual de pares: " + (vetorA.length/100)*par);
-        System.out.print("Percentual de impares: " + (vetorA.length/100)*impar);
+        System.out.print("Percentual de pares: " + ((par*100)/vetorA.length));
+        System.out.print("Percentual de impares: " + ((impar*100)/vetorA.length));
     }
 }
