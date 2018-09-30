d2d5512d28a6595b9be911fc5dfb9b17a4b66347
Darlley <37590954+Darlley@users.noreply.github.com>
2018-09-30 19:14:28 -0400

    Create ex17.java

---
+++ Loiane Groner/Quarta Lista/ex17.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class ex71{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int, a=0, vetorA[] = new int[10];
+        
+        for(int i=0; i<=9; i++){
+            System.out.println("Informe uma idade: ");
+            vetor[i] = scan.nextInt();
+            
+            if(vetor[i] > 35){
+                a++;
+            }
+        }
+        
+        System.out.println("Quantidade de idades superiores a 35: " + a);    
+    }
+}
