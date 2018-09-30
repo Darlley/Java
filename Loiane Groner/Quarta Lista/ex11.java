d91cb6d990bc0844a49ef82a5a3729266a4bd32d
Darlley <37590954+Darlley@users.noreply.github.com>
2018-09-30 19:30:53 -0400

    ✔

--- Loiane Groner/Quarta Lista/ex11.java
+++ Loiane Groner/Quarta Lista/ex11.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
-public class ex01{
+public class ex11{
     public static void main(String[] args){
 
         Scanner scan = new Scanner(System.in);
         int par=0, vetorA[] = new int[10];
 
         for(int i=0; i<vetorA.length; i++){
+            System.out.print("Informe um valor: ");
             vetorA[i] = scan.nextInt();
+            
             if(vetorA[i] % 2 == 0){
                 par++;
             }
