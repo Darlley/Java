b73022dd2dc9c3e4a6a77914ffdfe0d10e3b843a
Darlley <37590954+Darlley@users.noreply.github.com>
2018-09-30 19:52:57 -0400

    ✔

--- Loiane Groner/Quarta Lista/ex18.java
+++ Loiane Groner/Quarta Lista/ex18.java
@@ -1,21 +1,34 @@
 import java.util.Scanner;
 public class ex18{
     public static void main(String[] args){
+        
         Scanner scan = new Scanner(System.in);
-        int, maior=0, menor=0, vetorA[] = new int[10];
+        int, indexMenor=0, indexMaior=0, maior=0, menor=0, vetorA[] = new int[10];
         
         for(int i=0; i<=9; i++){
             System.out.println("Informe uma idade: ");
             vetor[i] = scan.nextInt();
+        }
+        
+        indexMenor = 0;
+        menor = vetorA[0];
+        indexMaior = 0;
+        maior = vetorA[0];
+        
+        for(int i=1; i<vetorA.length; i++){
             
-            if(maior < vetor[i]){
-                maior = vetor[i];
-            }else if(vetor[i] < menor){
-              menor = vetor[i];
+            if(vetorA[i] > maior){
+                maior = vetorA[i];
+                indexMaior = i;
+            }else if(vetorA[i] < menor){
+                menor = vetor[i];
+                indexMenor = i;
             }
         }
         
         System.out.println("MAIOR: " + maior);
-        System.out.println("MENOR: " + menor);
+        System.out.println("Indice de MAIOR: " + indexMaior);
+        System.out.println("MAIOR: " + maior);
+        System.out.println("Indice de MENOR: " + indexMenor);
     }
 }
