22e0b693804f7f0fe162056f6b6b83869bfec3d7
Darlley <37590954+Darlley@users.noreply.github.com>
2018-10-30 19:45:27 -0400

    Update ex01.java

--- Disciplina de Programacao 1/8-Oitava Lista/ex01.java
+++ Disciplina de Programacao 1/8-Oitava Lista/ex01.java
@@ -1,6 +1,33 @@
 public class ex01{
   public static void main(Sting[] args){
-    int n, m, matriz[][];
-      
+    int l, c, matriz[][];
+    
+    l = input.readInt("Quantidade linhas: ");
+    c = input.readInt("Quantidade colunas: ");
+    
+    matrix = new int[l][c];
+    
+    for(int i=0; i<l; i++){
+      for(int j=0; j<c; i++){
+        matriz[i][j] = input.readInt("Informe um número ");
+      }
+    }
+    
+    int min = matriz[0][0];
+    int max = matriz[0][0];
+    
+    for(int i=0; i<l; i++){
+      for(int j=0; j<c; i++){
+        if(matriz[i][j] > max){
+          max = matriz[i][j];
+        }
+        if(matriz[i][j] < min){
+          min = matriz[i][j];
+        }
+      }
+    }
+    
+    Syste.out.println("Mínimo: " + min);
+    Syste.out.println("Máximo: " + max);
   }
 }
