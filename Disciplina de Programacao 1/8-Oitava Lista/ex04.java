bfb18c64aa06cf6e0a3e7a82dd6e5c82a952f916
Darlley <37590954+Darlley@users.noreply.github.com>
2018-10-30 21:39:24 -0400

    Create ex04.java

---
+++ Disciplina de Programacao 1/8-Oitava Lista/ex04.java
@@ -0,0 +1,23 @@
+public class ex04{
+  public static void main(String[] args){
+    int m, matrizA[][], matrizB[][], matrizC[][];
+    
+    m = Input.readInt("Quantidade linhas e colunas: ");
+    matrizA = matrizB = matrizC = new int[m][m];
+    
+    for(int i=0; i<m; i++){
+        for(int j=0; j<m; j++){
+		  System.out.println("ENTRADA: MATRIZ["+i+"]["+j+"]");
+		  matrizA[i][j] = Input.readInt("Informe um valor para a MatrizA["+i+"]["+j+"]: ");
+		  matrizB[i][j] = Input.readInt("Informe um valor para a MatrizB["+i+"]["+j+"]: ");
+        }
+    }
+	for(int i=0; i<m; i++){
+        for(int j=0; j<m; j++){
+			System.out.println("SAIDA: MATRIZ["+i+"]["+j+"]");
+			matrizC[i][j] = (matrizA[i][j] * matrizB[i][j]);
+			System.out.println(matrizC[i][j]);
+		}
+	}
+  }  
+}
