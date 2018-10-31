f57cc1b28453240fcd30cc923d38e4738612d4b8
Darlley <37590954+Darlley@users.noreply.github.com>
2018-10-30 20:18:53 -0400

    Create ex03.java

---
+++ Disciplina de Programacao 1/8-Oitava Lista/ex03.java
@@ -0,0 +1,23 @@
+public class ex03{
+  public static void main(Sting[] args){
+    int l, c, matrizA[][], matrizB[][];
+    
+    l1 = input.readInt("Quantidade linhas: ");
+    c1 = input.readInt("Quantidade colunas: ");
+    matrizA = new int[l1][c1];
+    
+    l2 = input.readInt("Quantidade linhas: ");
+    c2 = input.readInt("Quantidade colunas: ");
+    matrizB = new int[l2][c2];
+    
+    if(l1!=l2 || c1!=c2){
+      // MATRIZES DFERENTES!
+    }else if{
+      for(int i=0; i<l; i++){
+        for(int j=0; j<c; i++){
+          if(matrizA[i][j] != matrizB{i][j]{
+            // MATRIZES DFERENTES!
+          }
+        }
+      }  
+}
