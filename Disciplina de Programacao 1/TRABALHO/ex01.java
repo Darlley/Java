c6013edfa0a184c5f87aa18213dcac6f6ee789f3
Darlley <37590954+Darlley@users.noreply.github.com>
2018-11-27 13:58:36 -0300

    Update ex01.java

--- Disciplina de Programacao 1/TRABALHO/ex01.java
+++ Disciplina de Programacao 1/TRABALHO/ex01.java
@@ -1,31 +1,47 @@
 public class ex01{
-	public static void main(Sting[] args){
-		int m, n, modelo[][], verifica[][];
-		modelo = new int[9][9];
-		verifica = new int[3][3];
+	public static void main(String args[]){
+		int m0 [][] = new int[9][9];
+		int m1 [][] = new int[3][3];
+		int x=0,i,j;
 		
-		// PRIMEIRA MATRIZ 
-		for(int i=0; i<=8;i++){
-			for(int j=0; j<=8;i++){
-				modelo[i][j] = Input.readInt("Informe o valor de ["+i+"]["+j+"]: ");
-			}				
-		}
-		
-		// SEGUNDA MATRIZ
-		System.out.print("VERIFICA SE UM DETERMINADO PADRÃO EXISTE NA MATRIZ!");	
-		for(int i=0;i<=2;i++){
-			for(int j=0;j<=2;j++){
-				verifica[i][j] = Input.readInt("Informe o valor de ["+i+"]["+j+"]: ");
+		for(i=0;i<8;i++){
+			for(j=0;j<8;j++){
+				m0[i][j] = (int)(Math.random()*2);
+				System.out.println(m0[i][j]);
+				
 			}
 		}
 		
-		// VERIFICAÇÃO
-		for(int i=0;i<=8;i++){
-			for(int j=0;j<=8;j++){
-				while(modelo[i][j] != verifica[i][j]){
-				
+		for(i=0;i<2;i++){
+			for(j=0;j<2;j++){
+				m1[i][j] = m0[i][j];
+			}
+		}
+		
+		for(i=0;i<2;i++){
+			for(j=0;j<2;j++){
+				if(m0[i][j]==m1[i][j]){
+					x = x + 1;
+				}
+				if(m0[i][j+3]==m1[i][j]){
+					x = x + 1;
+				}
+				if(m0[i+3][j]==m1[i][j]){
+					x = x + 1;
+				}
+				if(m0[i][j+6]==m1[i][j]){
+					x = x + 1;
+				}
+				if(m0[i+6][j]==m1[i][j]){
+					x = x + 1;
 				}
 			}
+		}
+		
+		if(x==5){
+			System.out.println("Padrao encontrado");
+		}else{
+			System.out.println("Padrao nao encontrado");
 		}
 	}
 }
