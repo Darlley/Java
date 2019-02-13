18eb77e02573932493dfef58b7642e7c93afd29a
Darlley <37590954+Darlley@users.noreply.github.com>
2019-02-13 14:06:22 -0300

    Update ex01.java

--- Disciplina de Programacao 2/Lista 1/ex01.java
+++ Disciplina de Programacao 2/Lista 1/ex01.java
@@ -2,3 +2,21 @@
 e apresentá-la convertida em graus Fahrenheit. A fórmula de conversão
 é: F <- (9 * C + 160) / 5, sendo F a temperatura em Fahrenheit e C
 a temperatura em Celsius.*/
+
+public class exemplo1{
+
+	// C R I A R  M É T O D O  F O R M U L A
+	public static void formula (double c){
+		double fahrenheit;
+		fahrenheit = (9 * c + 160)/5;
+		System.out.println(c + "º Celsius convertido para Fahrenheit é igual a " + fahrenheit + "º."); 
+	}
+
+	// M É T O D O  P R I N C I P A L
+	public static void main(String[] args){
+		double celsius;
+		celsius = Input.readDouble("Informe a temperatura: ");
+		formula(celsius);
+	}
+
+}
