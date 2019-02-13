d9f1cf240714aa1a820e3da8b536ea56601efbff
Darlley <37590954+Darlley@users.noreply.github.com>
2019-02-13 14:14:29 -0300

    Update ex01.java

--- Disciplina de Programacao 2/Lista 1/ex01.java
+++ Disciplina de Programacao 2/Lista 1/ex01.java
@@ -1,5 +1,4 @@
-/*1) Desenvolva um algoritmo para ler uma temperatura em graus Celsius e apresentá-la convertida em graus Fahrenheit. A fórmula de conversão
-é: F <- (9 * C + 160) / 5, sendo F a temperatura em Fahrenheit e C a temperatura em Celsius.*/
+/*1) Desenvolva um algoritmo para ler uma temperatura em graus Celsius e apresentá-la convertida em graus Fahrenheit. A fórmula de conversão é: F <- (9 * C + 160) / 5, sendo F a temperatura em Fahrenheit e C a temperatura em Celsius.*/
 
 public class exemplo1{
 
