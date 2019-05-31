94a04e092343bf844baaa65f17535d861f18414e
Darlley <37590954+Darlley@users.noreply.github.com>
2019-05-30 20:10:57 -0400

    esqueleto do algoritmo

--- Disciplina de Programacao 2/Lista 6/ex02.java
+++ Disciplina de Programacao 2/Lista 6/ex02.java
@@ -18,3 +18,111 @@ professor que recebe o maior salário e o professor que tem a maior idade.
 Para finalizar construa um método para retornar o curso mais antigo de todos
 os cursos cadastrados.
 */
+
+public class UCDB {
+    
+    public String endereço;
+    public String numCursos[][] = new String[4][30];
+    public String numProfessores[][] = new String[6][100];
+    public int anoFundação;
+    
+    // C O N S T R U T O R
+    public UCDB(){}
+    public void criarCurso(){
+        String nomeCurso, areaCurso, dataInicioLetivo; //saúde, exatas, humanas
+        int numVagas;
+
+        for(int i=0; i<=30; i++){
+            for(int j=0;j<=30; j++){
+                numCursos[i][j].nomeCurso = Input.readString("");
+                areaCurso = Input.readString("");
+                dataInicioLetivo = Input.readString("");
+                numVagas = Input.readInt("");
+            }
+        }        
+    }
+    public void cadastrarProfessor(){
+        String nome, endereco, telefone;
+        int registro, idade;
+        double salario;
+        
+        for(int i=0; i<=100; i++){
+        
+        }
+    }
+    
+    /*
+    public int getMatricula(){
+        return matricula;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public int getDepartamento(){
+        return departamento;
+    }
+    public double getSalario(){
+        return salário;
+    }
+    public String getFuncao(){
+        return função;
+    }
+    */
+    
+    // LISTAR INFORMAÇÃO DO PROFESSOR
+    public void infoProfessor(){
+        
+    }
+    // LISTAR INFORMAÇÃO DO CURSO
+    public void infoCurso(){
+    
+    }
+    public void maiorSalario(){
+    }
+    public void maiorIdadeProfessor(){
+    }
+    public void maiorIdadeCurso(){
+    }
+    
+    public static void main(String[] args) {
+        int qtdCursos, qtdProfessores, continuar = 1, opcao;
+        UCDB cursos, professores;
+        cursos = new UCDB();
+        professores = new UCDB();
+        
+        while(continuar == 1){
+            System.out.println("\n= = = = = = = = = MENU = = = = = = = = =");
+            System.out.println("Digite\n[1] para inserir funcionário;\n[2] Para Pesquisar salário com nome;\n[3] para calcular valor total da folha de pagamento;\n[4] Para saber maior salário;\n[5] Para pesquisar funcionários pelo departamento;\n[6] Para pesquisar funcionarios pelas funções\n[7] Para imprimir dados dos funcionários;\n[ENTER] Para sair;\n= = = = = = = = = = = = = = = = = =");
+            opcao = Input.readInt();
+        
+            switch(opcao){
+                case 1: //cadastrar cursos
+                    cursos.criarCurso();
+                    break;
+                case 2: //cadastrar professores
+                    
+                    break;
+                case 3: //consultar cursos pela área
+                    
+                    break;
+                case 4: //consultar professores por salário
+                    
+                    break;
+                case 5: //consultar professores por idade
+                    
+                    break;
+                case 6: //calcular gasto mensal com professores
+                    
+                    break;
+                case 7: //mostrar cursos
+                   
+                    break;
+                case 8: //mostrar professores
+                    
+                    break;
+                default:
+                    System.out.println("Saindo...");
+            }
+        }
+    }
+}
