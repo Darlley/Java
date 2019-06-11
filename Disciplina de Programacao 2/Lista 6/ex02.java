cd173c6c38dc7c2b2b09a51d729a5ebd57f38aec
Darlley <37590954+Darlley@users.noreply.github.com>
2019-06-11 15:04:42 -0400

    Update ex02.java

--- Disciplina de Programacao 2/Lista 6/ex02.java
+++ Disciplina de Programacao 2/Lista 6/ex02.java
@@ -1,22 +1,5 @@
 /*
-Escreva uma classe chamada UCDB. A UCDB possui como características
-endereço, ano de fundação, um número de pelo menos 30 diferentes cursos
-e pelo menos 100 professores. Cada curso possui como atributo um nome,
-uma área de atuação (saúde, exatas, humanas), uma data de início de
-atividades e número de vagas abertas para entrada no vestibular. Já os
-professores possuem como característica um nome, um registro de
-funcionário, idade, endereço, telefone e salário. Construa uma main que
-represente a UCDB e seja capaz de cadastrar cursos e professores. Construa
-um construtor que não recebe argumentos para professores e cursos.
-Construa um construtor que receba argumentos para professores e cursos.
-Construa uma função que a partir da UCDB o usuário consiga listar as
-informações dos professores e outra para listar as informações dos cursos.
-Além disso, permita consultas aos cursos por área, aos professores por
-salário e idade. Faça ainda uma funcionalidade que forneça o gasto mensal
-da UCDB com os professores cadastrados. Faça um método para divulgar o
-professor que recebe o maior salário e o professor que tem a maior idade.
-Para finalizar construa um método para retornar o curso mais antigo de todos
-os cursos cadastrados.
+Escreva uma classe chamada UCDB. A UCDB possui como características endereço, ano de fundação, um número de pelo menos 30 diferentes cursos e pelo menos 100 professores. Cada curso possui como atributo um nome, uma área de atuação (saúde, exatas, humanas), uma data de início de atividades e número de vagas abertas para entrada no vestibular. Já os professores possuem como característica um nome, um registro de funcionário, idade, endereço, telefone e salário. Construa uma main que represente a UCDB e seja capaz de cadastrar cursos e professores. Construa um construtor que não recebe argumentos para professores e cursos. Construa um construtor que receba argumentos para professores e cursos. Construa uma função que a partir da UCDB o usuário consiga listar as informações dos professores e outra para listar as informações dos cursos. Além disso, permita consultas aos cursos por área, aos professores por salário e idade. Faça ainda uma funcionalidade que forneça o gasto mensal da UCDB com os professores cadastrados. Faça um método para divulgar o professor que recebe o maior salário e o professor que tem a maior idade. Para finalizar construa um método para retornar o curso mais antigo de todos os cursos cadastrados.
 */
 
 public class Curso { 
