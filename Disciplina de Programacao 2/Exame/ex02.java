9282a9efd67b9b35026d7b19c2992ed278e6e2e5
Darlley <37590954+Darlley@users.noreply.github.com>
2019-07-01 16:00:49 -0400

    ✔

--- Disciplina de Programacao 2/Exame/ex02.java
+++ Disciplina de Programacao 2/Exame/ex02.java
@@ -17,13 +17,13 @@ public class Empregado {
     
     //METODOS SETTERS
     public void setNome(String nome){
-        this.primeiroNome = primeiroNome;
+        this.primeiroNome = nome;
     }
-    public void setSobrenome(String capital){
+    public void setSobrenome(String sobrenome){
         this.sobrenome = sobrenome;
     }
-    public void setSalario(double dimensao){
-        this.sobrenome = sobrenome;
+    public void setSalario(double salario){
+        this.salarioMensal = salario;
     }
         
     //METODOS GETTERS
@@ -40,6 +40,12 @@ public class Empregado {
     public void calcularSalario(Empregado funcionarios){
         Double total;
         total = funcionarios.getSalario() * 12;
+        System.out.println("TOTAL SALÁRIO: " + total);
+    }
+    public void aumento(Empregado funcionarios){
+        double aumento = funcionarios.getSalario();
+        aumento += funcionarios.getSalario()*10/100;
+        funcionarios.setSalario(aumento); 
     }
     
     public static void main(String[] args) {
@@ -47,28 +53,63 @@ public class Empregado {
         Empregado funcionarios[] = new Empregado[2];
         
         while(ligado != 0){
-            leitor = Input.readInt("1- Para cadastrar funcionário;\n2- Para calcular o valor anual do salario;\n3- Para mostrar;\nESCOLHA: ");
+            System.out.println("=====  M E N U  =====");
+            leitor = Input.readInt("1- Para cadastrar funcionário;\n2- Para calcular o valor anual do salario;\n3- Aumentar salário em 10%;\n4- Para mostrar;\nESCOLHA: ");
+            System.out.println("");
             switch(leitor){
                 case 1:
+                    System.out.println("===== CADASTRAR FUNCIONÁRIO =====\n");
+                    System.out.println("- - - - - - - - - - - - -");
+                    System.out.println("| LIMITE DE CADASTROS: 2 |\n| VOCÊ JA CADASTROU: " + contadorFuncionarios + "   |\n- - - - - - - - - - - - -");
                     if(contadorFuncionarios < 2){
+                        System.out.println("");
                         funcionarios[contadorFuncionarios] = new Empregado();
-                        
+                        contadorFuncionarios = contadorFuncionarios+1;  
                     }
-                    contadorFuncionarios = contadorFuncionarios+1;
+                    
+                    System.out.println("");
                     break;
                 case 2:
                     int indice;
+                    System.out.println("===== CALCULAR SALÁRIO ANUAL =====\n");
                     if(contadorFuncionarios != 0){
-                        for(int i=0; i<2;i++){
-                            System.out.println("\nTotal anual do salário de " + funcionarios[i].getNome());
+                        for(int i=0; i<contadorFuncionarios;i++){ // Enquanto indice for menor ou igual que o total de funcionarios
+                            System.out.println("NOME FUNCIONÁRIO: " + funcionarios[i].getNome() + " " + funcionarios[i].getSobrenome());
                             funcionarios[i].calcularSalario(funcionarios[i]);
+                            if((i+1)<contadorFuncionarios){ // Se tiver mais, quebrar linha
+                                System.out.println("- - - - - - - - - -");
+                            }
                         }
                     }
+                    System.out.println("");
                     break;
-                case 3: // PARA TESTAR
-                    for(int i=0; i<2; i++){
-                        System.out.println(funcionarios[i].primeiroNome);
+                case 3: // AUMENTO DE 10%
+                    System.out.println("===== AUMENTAR 10% =====\n");
+                    for(int i=0; i<contadorFuncionarios; i++){
+                        System.out.println("NOME FUNCIONÁRIO: " + funcionarios[i].getNome() + " " + funcionarios[i].getSobrenome());
+                        System.out.println("ANTIGO SALÁRIO: " + funcionarios[i].getSalario());
+                        funcionarios[i].aumento(funcionarios[i]);
+                        System.out.println("NOVO SALÁRIO: " + funcionarios[i].getSalario());
+                        if((i+1)<contadorFuncionarios){ // Se tiver mais, quebrar linha
+                            System.out.println("- - - - - - - - - -");
+                        }
                     }
+                    System.out.println("");
+                    break;
+                case 4: // MOSTRAR FICHA
+                    System.out.println("===== FUNCIONÁRIOS =====\n");
+                    System.out.println("------------");
+                    System.out.println("| TOTAL: " + contadorFuncionarios + " |\n------------");
+                    for(int i=0; i<contadorFuncionarios; i++){
+                        System.out.println("NOME: " + funcionarios[i].getNome());
+                        System.out.println("SOBRENOME: " + funcionarios[i].getSobrenome());
+                        System.out.println("SALÁRIO: " + funcionarios[i].getSalario());
+                        System.out.println();
+                        if((i+1)<contadorFuncionarios){ // Se tiver mais, quebrar linha
+                            System.out.println("- - - - - - - - - -");
+                        }
+                    }
+                    System.out.println("");
                     break;
                 default:
                     ligado = 0;
