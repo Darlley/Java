fd8921b7847c97a9f577c0e1e83e055540c15605
Darlley <37590954+Darlley@users.noreply.github.com>
2019-08-08 22:18:14 -0400

    Create ex01.java

---
+++ Disciplina de Paradigmas de Programação/avaliação/ex01.java
@@ -0,0 +1,21 @@
+// M O D E L O  F I L M E
+
+package model;
+public class filmeM {
+    
+}
+
+
+
+// M O D E L O  F I L M E  O U  S E R I E
+
+
+
+// M A I N
+
+package model;
+import model.FilmeSerieM;
+
+public class FilmeSerieM {
+   filmeM filme = new filmeM(); 
+}
