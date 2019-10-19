3caa12044ab39c6a62c8bb65348361062d9aaee1
Darlley <37590954+Darlley@users.noreply.github.com>
2019-10-18 20:38:25 -0400

    Create noLista.java

---
+++ TADS-ED/ListasSimplesmenteEncadeadas/noLista.java
@@ -0,0 +1,23 @@
+package ListasSimplesmenteEncadeada;
+
+public class noLista {
+    private int chave;
+    private noLista prox;
+
+    public int getChave() {
+        return chave;
+    }
+
+    public void setChave(int chave) {
+        this.chave = chave;
+    }
+
+    public noLista getProx() {
+        return prox;
+    }
+
+    public void setProx(noLista prox) {
+        this.prox = prox;
+    }
+ 
+}
